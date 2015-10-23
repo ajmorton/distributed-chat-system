af228c37266267003784ee752e95bbf38308995f
ajmorton <ajmorton2@gmail.com>
2015-10-23 13:23:25 +1100

    replace word doc with Report Skeleton.rtf

--- commands/NewIdentity.java
+++ commands/NewIdentity.java
@@ -31,7 +31,6 @@ public class NewIdentity extends Command{
 		
 		if(former.equals("")){
 			// first name assignment by server, say nothing
-			System.out.println("name from blank");
 			c.setClientName(identity);
 				
 		} else if(former.equals(identity)){
